d92d08f265d11edaf976b82b53ea0fbb62ef2904
kein <kein10119810@gmail.com>
2025-03-02 00:02:03 +0900

    jissou

--- Iphone2/src/Iphone.java
+++ Iphone2/src/Iphone.java
@@ -2,8 +2,11 @@
 public class Iphone {
 
 	public static void main(String[] args) {
-
-
+		SmartPhone iphone =new SmartPhone();
+		iphone.play();
+		iphone.stop();
+		iphone.next();
+		iphone.back();
 	}
 
 }
